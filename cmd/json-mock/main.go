52333287a134d6e890f18501d320ca49e409b627
Tsukasa Arima <pyar6329@gmail.com>
2020-03-03 15:52:00 +0900

    add cors origin

--- cmd/json-mock/main.go
+++ cmd/json-mock/main.go
@@ -125,8 +125,12 @@ func main() {
 }
 
 func Response(w http.ResponseWriter, r *http.Request) {
+    or, ret := os.LookupEnv("CORS_ORIGIN")
+    if ret == false {
+        or = "*"
+    }
 
-	w.Header().Set( "Access-Control-Allow-Origin", "*" )
+	w.Header().Set( "Access-Control-Allow-Origin", or )
 	w.Header().Set( "Access-Control-Allow-Credentials", "true" )
 	w.Header().Set( "Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization" )
 	w.Header().Set( "Access-Control-Allow-Methods","GET, POST, PUT, DELETE, OPTIONS" )
